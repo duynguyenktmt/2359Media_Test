cee6d5bb3a320feb28b24ae511b3dc90d5a06e67
Duy Nguyen <Duy.NguyenPhamAnh@vn.bosch.com>
2020-02-13 16:46:16 +0700

    commit

--- test/src/main/java/com/example/demo/Controller.java
+++ test/src/main/java/com/example/demo/Controller.java
@@ -1,5 +1,35 @@
 package com.example.demo;
 
-public class Controller {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.animal.model.Animal;
+import com.example.demo.animal.model.AnimalDAO;
+
+@RestController
+public class Controller {
+	@Autowired
+    private AnimalDAO animalDAO;
+	
+	@RequestMapping("/")
+    @ResponseBody
+    public String welcome() {
+        return "Welcome to RestTemplate Example.";
+    }
+	
+	@RequestMapping(value = "/employees", //
+            method = RequestMethod.GET, //
+            produces = { MediaType.APPLICATION_JSON_VALUE, //
+                    MediaType.APPLICATION_XML_VALUE })
+	@ResponseBody
+    public List<Animal> getAllAnimals() {
+        List<Animal> list = animalDAO.getAllAnimals();
+        return list;
+    }
 }
